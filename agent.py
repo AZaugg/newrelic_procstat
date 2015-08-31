e7f5ababe04002a803528e654b6e4a1e3fcbc78d
Andy Zaugg <andy.zaugg@gmail.com>
2015-08-31 21:40:39 +1000

    initial commit

---
+++ agent.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python
+
+import psutil
+
+import requests
+import json
+import subprocess
+import logging
+from socket import gethostname
+
+# TODO: Migrate away from psutil to procfs
+# TODO: Make named tuple for metrics
+# TODO: use loggin
+# TODO: Daemonise process
+# TODO: Disk stats
+# TODO: Metadata metrics to newrelic
+
+
+URL = "https://platform-api.newrelic.com/platform/v1/metrics"
+GUID = ""
+LICENSE = ""
+
+class metric(object):
+    def __init__(self, t_class):
+
+        # t_class: [CPU|VM|NET|DISK]
+        # unit:    [PERCENTAGE|COUNT|...]
+
+        self.t_class = t_class
+        self.metrics = []
+
+    def add_metric(self, unit, name, metric, sub_class=None):
+        self.metrics.append((name, metric, sub_class, unit))
+
+#-----------------------------------------------------------------------------------------------------------
+def run_process(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+    output = process.communicate()[0].split('\n')
+    output = [line for line in output if line != '']
+
+    return output
+#-----------------------------------------------------------------------------------------------------------
+def get_cpu_stats(process):
+
+    cpustats = metric('cpu')
+
+    csw = process.num_ctx_switches()
+    cpustats.add_metric('count', 'v_csw', csw.voluntary, 'csw')
+    cpustats.add_metric('count', 'i_csw', csw.involuntary, 'csw')
+    cpustats.add_metric('count', 'threads', process.num_threads())
+
+    # Pidstat to get CPU time of process
+    '''
+    pidstat -p 3736
+    Linux 2.6.32-504.8.1.el6.x86_64 (localhost.localdomain)     29/08/15    _x86_64_    (2 CPU)
+
+    04:47:53          PID    %usr %system  %guest    %CPU   CPU  Command
+    04:47:53         3736    0.00    0.00    0.00    0.00     1  sshd
+    '''
+
+    cmd = ["pidstat", "-p"]
+    cmd.append(str(process.pid))
+
+    #process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    #out = process.communicate()[0].split('\n')
+    output = run_process(cmd)
+
+    line_matrix = {}
+    for line in output:
+        if 'usr' in line and 'sys' in line:
+            items = line.split()
+
+            line_matrix['usr'] = items.index('%usr')
+            line_matrix['sys'] = items.index('%system')
+
+            continue
+
+        if line_matrix:
+            for item in line_matrix:
+                cpustats.add_metric('percentage', item, line[line_matrix[item]], 'utilization')
+
+    return  cpustats
+
+#-----------------------------------------------------------------------------------------------------------
+def get_net_stats(process):
+
+    # TODO: bytes in/out per process /proc/<PID>/net/dev
+    # TODO: errors in/out per process /proc/<PID>/net/dev
+
+    netstats = metric('net')
+    current_connections = process.connections()
+
+    connection_types = dict()
+    for connection in current_connections:
+        if connection.status not in connection_types.keys():
+            connection_types[connection.status] = 1
+            continue
+
+        connection_types[connection.status] += 1
+
+    for item in connection_types:
+        netstats.add_metric('count', item, connection_types[item], 'connections')
+
+    return netstats
+
+#-----------------------------------------------------------------------------------------------------------
+def get_vm_stats(process):
+    memstats = metric('mem')
+
+    memstats.add_metric('percentage', 'percentage_usage', process.memory_percent())
+
+    mem_info = process.memory_info_ex()
+    try:
+        memstats.add_metric('count', 'rss', mem_info.rss, 'usage')
+        memstats.add_metric('count', 'vms', mem_info.vms, 'usage')
+        memstats.add_metric('count', 'shared', mem_info.shared, 'usage')
+        memstats.add_metric('count', 'text', mem_info.text, 'usage')
+        memstats.add_metric('count', 'lib', mem_info.lib, 'usage')
+        memstats.add_metric('count', 'data', mem_info.data, 'usage')
+        memstats.add_metric('count', 'dirty', mem_info.dirtya, 'usage')
+    except AttributeError:
+        # Not all fields are there
+        pass
+
+    '''
+    pidstat -r -p 2736
+    Linux 3.13.0-29-generic (ubuntu)    31/08/15    _x86_64_    (1 CPU)
+
+    20:23:58      UID       PID  minflt/s  majflt/s     VSZ    RSS   %MEM  Command
+    20:23:58     1000      2736      0.11      0.00   22440   3720   0.74  bash
+    '''
+
+    cmd = ["pidstat", "-r", "-p"]
+    cmd.append(str(process.pid))
+    output = run_process(cmd)
+
+    line_matrix = {}
+    for line in output:
+        if 'minflt/s' in line and 'majflt/s' in line:
+            items = line.split()
+
+            line_matrix['minflts'] = items.index('minflt/s')
+            line_matrix['majflts'] = items.index('majflt/s')
+
+            continue
+
+        if line_matrix:
+            for item in line_matrix:
+                memstats.add_metric('count', item, line[line_matrix[item]], 'faults')
+
+    return memstats
+
+#-----------------------------------------------------------------------------------------------------------
+def get_io_stats(process):
+    pass
+#-----------------------------------------------------------------------------------------------------------
+
+
+collection = []
+metrics = dict()
+process = psutil.Process(2683)
+
+collection.append(get_cpu_stats(process))
+collection.append(get_net_stats(process))
+collection.append(get_vm_stats(process))
+#collection.append(get_io_stats(process))
+
+for metric in collection:
+    for data in metric.metrics:
+        namespace = ''
+        unit = ''
+        section = metric.t_class
+        name = data[0]
+        if data[2]:
+            namespace = data[2] + "/"
+
+        if data[3]:
+            unit = "[" + data[3] + "]"
+
+        #key = "Component/%s%s/%s%s" % (metric.t_class, namespace, data[0])
+        key = "Component/%(section)s/%(namespace)s%(name)s%(unit)s" % locals()
+        value = data[1]
+
+        metrics[key] = value
+
+print metrics
+a = {
+  "agent": {
+    "host" : gethostname(),
+    "pid" : 1234,
+    "version" : "1.0.0"
+  },
+  "components": [
+    {
+      "name": gethostname(),
+      "guid": "",
+      "duration" : 60,
+      "metrics" : metrics
+    }
+  ]
+}
+
+headers = {"X-License-Key": LICENSE, "Content-Type":"application/json", "Accept":"application/json"}
+
+r = requests.post(url=URL, headers=headers, data=json.dumps(a))
+print r.status_code
+print r.text
